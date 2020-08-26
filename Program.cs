878911486c49e181c354b251f55e4ac68acd8ffd
irwinet <irwinestradatorres@gmail.com>
2020-08-25 19:59:36 -0500

    Profits And Hours Employee

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CSharp01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float tasa = 0;
+            int hrs = 0;
+            int hrsRegulares = 0;
+            int hrsExtras = 0;
+            float gRegulares = 0;
+            float gExtras = 0;
+            float gTotales = 0;
+
+            Console.WriteLine("Hours:");
+            hrs = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Tasa:");
+            tasa = Convert.ToSingle(Console.ReadLine());
+
+            if(hrs>40){
+                hrsExtras = hrs - 40;                                
+            }
+
+            hrsRegulares = hrs - hrsExtras;
+            gRegulares = hrsRegulares * tasa;
+            gExtras = hrsExtras * 1.5f * tasa;
+            gTotales = gRegulares + gExtras;
+
+            Console.WriteLine($"Number of Hours Regulars: {hrsRegulares}");
+            Console.WriteLine("Number of Hours Extras: "+ hrsExtras);
+
+            Console.WriteLine("Profits Brutas Regulars: "+ gRegulares);
+            Console.WriteLine("Profits Brutas Extras: "+ gExtras);
+            Console.WriteLine("Profits Totales: "+ gTotales);
+
+            Console.ReadLine();
+        }
+    }
+}
